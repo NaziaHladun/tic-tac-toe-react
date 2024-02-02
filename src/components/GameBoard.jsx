10f757560f47a96c477369756c2ae887a90cc9b2
NaziaHladun <fabulous7351@gmail.com>
2024-02-02 21:52:09 +0200

    added player selection

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
+
 const initialBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelect, activePlayerSymbol }) {
+  const [board, setBoard] = useState(initialBoard);
+
+  function handleSelect(rowIndex, colIndex) {
+    setBoard((prevBoard) => {
+      const updateBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      updateBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updateBoard;
+    });
+
+    onSelect();
+  }
+
   return (
     <ol id="game-board">
-      {initialBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelect(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
